a86479a5113d931a072586a4ce9cbc592f64fb49
Zar-Rok <zar_rok@live.fr>
2023-11-28 23:31:16 +0100

    Try to use the same names as the initial setup

--- sklearn-baseline/sklearn_baseline.py
+++ sklearn-baseline/sklearn_baseline.py
@@ -6,37 +6,60 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 import os
 import joblib
-from sklearn_baseline_train import load_data, preprocess
+import numpy as np
+from baseline_train import load_data, preprocess_text_dataset, preprocess_url_dataset
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Webpage classification with sklearn pipeline')
-    parser.add_argument("-i", "--input_data", help="Path to the jsonl file for which predictions should be made.", required=True)
-    parser.add_argument("-m", "--model", help="The sklearn SGDClassifier model to use for the predictions.", required=True)
-    parser.add_argument("-o", "--output", help="Path to the directory to write the results to.", required=True)
+    parser = argparse.ArgumentParser(
+        description='Webpage classification with sklearn pipeline')
+    parser.add_argument(
+        "-i", "--input_data", help="Path to the jsonl file for which predictions should be made.", required=True)
+    parser.add_argument(
+        "-m", "--model", help="The sklearn SGDClassifier model to use for the predictions.", required=True)
+    parser.add_argument(
+        "-o", "--output", help="Path to the directory to write the results to.", required=True)
     return parser.parse_args()
+
 
 def load_model(model_file):
     # Load the trained model
-    pipeline = joblib.load(model_file)
-    return pipeline
+    models = joblib.load(model_file)
+    return models
+
 
 def main(input_file, output_dir, model_file):
+    # Load models
+    models = load_model(model_file)
+    pipeline_text = models['text']
+    pipeline_url = models['url']
+    corpus = models['url_corpus']
+    model_final = models['final']
+
     # Load datasets
     test_data = load_data(input_file)
 
-    # Preprocess the content
-    test_data['url'] = test_data['url'].apply(preprocess)
+    # Text based model
+    test_features1 = preprocess_text_dataset(test_data)
+    test_predictions1 = pipeline_text.predict_proba(test_features1)
 
-    # Feature extraction and classifier pipeline
-    pipeline = load_model(model_file)
-    
-    # Make predictions on the test data
-    test_predictions = pipeline.predict(test_data['url'])
+    # Url based model
+    test_features2, corpus = preprocess_url_dataset(
+        test_data, train=False, corpus=corpus)
+    test_predictions2 = pipeline_url.predict_proba(test_features2)
+
+    # Final model
+    test_features_final = np.concatenate(
+        [test_predictions1, test_predictions2], axis=1)
+    model_final = SGDClassifier()
+    test_predictions_final = model_final.predict(test_features_final)
 
     # Save the predictions
-    test_data['prediction'] = test_predictions
+    test_data['prediction'] = test_predictions_final
     output_path = os.path.join(output_dir, 'predictions.jsonl')
-    test_data[['uid', 'prediction']].to_json(output_path, orient='records', lines=True)
+    test_data[['uid', 'prediction']].to_json(
+        output_path, orient='records', lines=True)
+
 
 if __name__ == "__main__":
     args = parse_args()
